5b625c35ac8a5bb1e5ff1a04ef6214ca021b7a88
AndresContreras1034 <contrerandres001@gmail.com>
2025-10-06 07:24:23 -0500

    Actualización de configuración y dependencias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuración de Vite para GitHub Pages
 export default defineConfig({
   plugins: [react()],
+  base: '/AndresContreras1034.io/', 
 })
